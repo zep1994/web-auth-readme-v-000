aff09767e6d5cdd208fa2e26fb4dcc403c0d400c
zep1994 <snaxbags@protonmail.com>
2018-11-17 14:14:18 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
 
+  get '/auth', to: 'sessions#create'
+  get '/friends', to: 'searches#friends'
+
 end
